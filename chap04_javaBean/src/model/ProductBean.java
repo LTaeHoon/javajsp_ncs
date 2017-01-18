a91f4844567b97563dd5005cef117d38e933276e
LTaeHoon <lth1011@gmail.com>
2017-01-18 16:39:49 +0900

    java jsp_서블릿이용 MVC 모델 적용 코드

---
+++ chap04_javaBean/src/model/ProductBean.java
@@ -0,0 +1,73 @@
+package model;
+
+public class ProductBean {
+	private String sabun;
+	private String saname;
+	private String procode;
+	private String proname;
+	private int price;
+	private String image;
+	/* 사번과 이름 DB */
+	final String dbbun[] = {"k201701","k201702","k201703"};
+	final String dbname[] = {"홍길동", "이순신", "유관순"};
+	
+	public boolean check(){
+		for(int i = 0; i < dbbun.length; i++){
+			if(sabun.equals(dbbun[i]) && saname.equals(dbname[i])){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	public String getSabun() {
+		return sabun;
+	}
+
+	public void setSabun(String sabun) {
+		this.sabun = sabun;
+	}
+
+	public String getSaname() {
+		return saname;
+	}
+
+	public void setSaname(String saname) {
+		this.saname = saname;
+	}
+
+	public String getProcode() {
+		return procode;
+	}
+
+	public void setProcode(String procode) {
+		this.procode = procode;
+	}
+
+	public String getProname() {
+		return proname;
+	}
+
+	public void setProname(String proname) {
+		this.proname = proname;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
